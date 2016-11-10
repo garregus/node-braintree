aba71204601c640140d8cfb2bbc813949d11274d
garregus <garregus@gmail.com>
2016-11-10 02:07:23 -0600

    listen ughghghghg

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(process.env.PORT){
-  console.log("Live at Port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
